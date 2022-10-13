89e34359af6e935eae75dfa8becb788ef5e6ef6a
Efrain_R_Ruiz <79539402+rure0208@users.noreply.github.com>
2022-10-13 08:34:19 -0700

    creacion de proyecto

---
+++ pages/calcular.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './components/Layout'
+
+const Calcular = () => {
+  return (
+    <Layout tituloPagina="calcular">
+
+    </Layout>
+  )
+}
+
+export default Calcular
